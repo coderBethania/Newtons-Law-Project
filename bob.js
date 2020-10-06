72846f52b29da7ead4c04c36f1acbb10cd0a10bb
coderBethania <70826102+coderBethania@users.noreply.github.com>
2020-10-06 19:52:03 -0400

    Add files via upload

--- bob.js
+++ bob.js
@@ -1,7 +1,7 @@
 class Bob{
     constructor(x, y) {
         var options = {
-          isStatic:true,
+         
             'restitution':0.3,
             'friction':0.5,
             'density':1.2
